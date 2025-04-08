0b89e1b0b4d8d355319089a589d982c14adf7e95
Villie <villie@dyn221215.cc.lehigh.edu>
2025-04-07 21:23:02 -0400

    update navbar font

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -24,6 +24,10 @@ export default function RootLayout({ children }) {
 					href="https://fonts.googleapis.com/css2?family=Anuphan:wght@400;500;700&display=swap"
 					rel="stylesheet"
 				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap"
+					rel="stylesheet"
+				/>
 			</head>
 			<body className={inter.className}>
 				{children}
